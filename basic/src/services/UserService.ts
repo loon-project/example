ca4b8c9f8c18953608d60f6b0f281baa3c42bfaf
vincent178 <vincent.007.cn@gmail.com>
2017-04-09 20:54:23 +0800

    fix

--- basic/src/services/UserService.ts
+++ basic/src/services/UserService.ts
@@ -1,11 +1,6 @@
 import {Service, Converter, ConnectionFactory, LogFactory, ApplicationLoader, Inject} from "typed-framework";
 import {User} from "../models/User";
 
-const template = {
-    uuid: 'id',
-    created_at: "createdAt"
-};
-
 @Service()
 export class UserService {
 
@@ -25,17 +20,11 @@ export class UserService {
             created_at: Date.now()
         };
 
+        // const member = await this.connection.select("*").from("members").where("id", 1);
 
-        const converter = new Converter(userFromDB, {template, returnType: User});
-
-        const user = converter.convert();
-
-        const member = await this.connection.select("*").from("members").where("id", 1);
-
-        this.logger.info(JSON.stringify(member));
 
         if (id === 1) {
-            return member;
+            return userFromDB;
         } else {
             throw new Error("not found");
         }
